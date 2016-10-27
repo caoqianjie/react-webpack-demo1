6d9f3b694997b2cdab1dae1cbffbd9061d9798b5
caoqianjie <785186047@qq.com>
2016-10-27 20:05:34 +0800

    'newsItem'

--- app/component/NewsItem.js
+++ app/component/NewsItem.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import url from 'url';
+import '../src/css/newsItem.css';
+import Moment from 'moment';
+import ImageGrayArrow from '../grayarrow.gif';
 class NewsItem extends React.Component {
+    getDomain() {
+        return url.parse(this.props.item.url).hostname;
+    }
+    getTitle() {
+        return (
+            <div className="newsItem-title">
+                <a className="newsItem-titleLink" href={this.props.item.url ? this.props.item.title : 'https://news.ycombinator.com/item?id=' + this.props.item.id}>{this.props.item.title}</a>
+                <span className="newsItem-domain"><a href={'https://news.ycombinator.com/from?site=' + this.getDomain()}>({this.getDomain()})</a></span>
+            </div>
+        )
+    }
+    getCommentLink() {
+        var commentText = 'discuss';
+        if(this.props.item.kids && this.props.item.kids.length) {
+            commentText = this.props.item.kids.length + ' comments';
+        }
+        return (
+            <a href={'https://news.ycombinator.com/item?id=' + this.props.item.id}>{commentText}</a>
+        );
+    }
+    getSubtext() {
+        return(
+            <div className="newsItem-subtext">
+                {this.props.item.score} points by <a href={'https://news.ycombinator.com/user?id=' + this.props.item.by}>{this.props.item.by}</a> {Moment.utc(this.props.item.time * 1000).fromNow()} | {this.getCommentLink()}
+            </div>
+        )
+    }
+    getRank() {
+        return (
+            <div className="newsItem-rank">
+                {this.props.rank}
+            </div>
+        );
+    }
+    getVote() {
+        return (
+            <div className="newsItem-vote">
+                <a href={'https://news.ycombinator.com/vote?for='+ this.props.item.id + '&dir=up&goto=news'}>
+                    <img src={ImageGrayArrow} width="10" />
+                </a>
+            </div>
+        );
+    }
     render() {
         return (
             <div className="newsItem">
-                I am newsItems
+                {this.getRank()}
+                {this.getVote()}
+                <div className="newsItem-itemText">
+                    {this.getTitle()}
+                    {this.getSubtext()}
+                </div>
             </div>
         );
     }
--- app/component/NewsList.js
+++ app/component/NewsList.js
@@ -3,10 +3,21 @@ import NewsHeader from './NewsHeader';
 import NewsItem from './NewsItem';
 class NewsList extends React.Component {
     render(){
+        let testData = {
+            "by" : "bane",
+            "descendants" : 49,
+            "id" : 11600137,
+            "kids" : [ 11600476, 11600473, 11600501, 11600463, 11600452, 11600528, 11600421, 11600577, 11600483 ],
+            "score" : 56,
+            "time" : 1461985332,
+            "title" : "Yahoo's Marissa Mayer could get $55M in severance pay",
+            "type" : "story",
+            "url" : "http://www.latimes.com/business/technology/la-fi-0429-tn-marissa-mayer-severance-20160429-story.html"
+        };
         return (
             <div className="newsList">
                 <NewsHeader/>
-                <NewsItem/>
+                <NewsItem item={testData} rank={1}/>
             </div>
         );
     }
